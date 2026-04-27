4bca8c39376ff1b94ae499086759a03b4265f875
nuvemmedicina <contato@nuvemmedicina.com.br>
2026-04-26 21:40:39 -0300

    fix: atualiza horário de funcionamento

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -198,7 +198,7 @@ export const CONTATO = {
   endereco:    'Rua Ceará, 600 – Sala 101',
   bairro:      'Santa Efigênia, Belo Horizonte – MG',
   cep:         'CEP 30150-310',
-  horario:     'Segunda a Sexta · 7h30 às 17h30',
+  horario:     'Segunda a Sexta · 7h às 17h30',
   maps:        'https://goo.gl/maps/BELYyu7yUHd41gdc8',
   instagram:   'https://instagram.com/nuvemmedicina',
   youtube:     'https://www.youtube.com/@NuvemMedicina',
